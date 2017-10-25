7d66441d4303e82a1b24435373c057a5427fca15
naroladhar <32682490+naroladhar@users.noreply.github.com>
2017-10-25 13:09:56 +0530

    Create Person.py

---
+++ Person.py
@@ -0,0 +1,50 @@
+class Person:
+    '''Class Person describe a person'''
+    personCount = 0
+    def __init__(self,name,dob,address)
+        '''
+        Objective : To initialize the objects of class function
+        Input variables
+            self(implicit parameter): Object of type Person
+            Name : string
+            Date of Birth (DOB) : string
+            Address : string
+        return value: none
+        '''
+        self.name=name
+        self.dob=dob
+        self.address=address
+        person.personCount+=1
+        
+    def getName(self)
+        '''
+        Objective : To retrieve name of an object 
+        Input variables:
+            self(implicit parameter): Object of type Person
+        return vale:none
+        '''
+        return self.name
+    
+    def getDob(self)
+        '''
+        Objective : To retrieve date of birth of an object
+        Input variables :
+            self(implicit parameter): Object of tupe Person
+        return value:None
+        '''
+        return self.dob
+    def getAddress(self)
+        '''
+        Objective : To retrieve addresss of an object
+        Input variables:
+            Self(implicit parameter) : 
+        '''    
+        return self.
+    def setName(self,name)    
+        '''
+        '''
+        return self.name=name
+    def setName(self,name)    
+        '''
+        '''
+        return self.name=name
